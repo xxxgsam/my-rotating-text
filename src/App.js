2f5c71b98b9a303e0384e87106fd1a3ca7170c00
Samuel Ge <sam@Sam.local>
2025-03-31 18:40:23 +0100

    Initial commit for RotatingText project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import RotatingText from './RotatingText';
+
+function App() {
+  return (
+    <div className="App">
+      <RotatingText
+        texts={['React', 'Bits', 'Is', 'Cool!']}
+        mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+        staggerFrom={"last"}
+        initial={{ y: "100%" }}
+        animate={{ y: 0 }}
+        exit={{ y: "-120%" }}
+        staggerDuration={0.025}
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        transition={{ type: "spring", damping: 30, stiffness: 400 }}
+        rotationInterval={2000}
+      />
+    </div>
+  );
+}
+
+export default App;
+
